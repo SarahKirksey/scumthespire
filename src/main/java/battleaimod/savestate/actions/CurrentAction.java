8142edcc277acda563864365641e30c81c5e905b
SarahKirksey <benjamindkirksey@gmail.com>
2021-05-08 17:34:33 -0700

    New CurrentActions

--- src/main/java/battleaimod/savestate/actions/CurrentAction.java
+++ src/main/java/battleaimod/savestate/actions/CurrentAction.java
@@ -1,20 +1,35 @@
 package battleaimod.savestate.actions;
 
 import battleaimod.fastobjects.actions.DiscardCardActionFast;
+import battleaimod.savestate.actions.untested.AttackFromDeckToHandActionState;
+import battleaimod.savestate.actions.untested.DiscardPileToTopOfDeckActionState;
+import battleaimod.savestate.actions.untested.ExhumeActionState;
+import battleaimod.savestate.actions.untested.SeekActionState;
+import battleaimod.savestate.actions.untested.SkillFromDeckToHandActionState;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DiscardAction;
 import com.megacrit.cardcrawl.actions.common.ExhaustAction;
+import com.megacrit.cardcrawl.actions.defect.SeekAction;
 import com.megacrit.cardcrawl.actions.unique.ArmamentsAction;
+import com.megacrit.cardcrawl.actions.unique.AttackFromDeckToHandAction;
+import com.megacrit.cardcrawl.actions.unique.DiscardPileToTopOfDeckAction;
 import com.megacrit.cardcrawl.actions.unique.DualWieldAction;
+import com.megacrit.cardcrawl.actions.unique.ExhumeAction;
+import com.megacrit.cardcrawl.actions.unique.SkillFromDeckToHandAction;
 
 import java.util.function.Function;
 
 public enum CurrentAction {
-    ARMAMENTS_ACTION(ArmamentsAction.class, action -> new ArmamentsActionState(action)),
+    ARMAMENTS_ACTION(ArmamentsAction.class, ArmamentsActionState::new),
     DISCARD_ACTION(DiscardAction.class, action -> new DiscardActionState((DiscardAction) action)),
-    DISCARD_ACTION_FAST(DiscardCardActionFast.class, action -> new DiscardActionState(action)),
-    DUAL_WIELD_ACTION(DualWieldAction.class, action -> new DualWieldActionState(action)),
-    EXHAUST_ACTION(ExhaustAction.class, action -> new ExhaustActionState(action));
+    DISCARD_ACTION_FAST(DiscardCardActionFast.class, DiscardActionState::new),
+    DREDGE_ACTION(DiscardPileToTopOfDeckAction .class, DiscardPileToTopOfDeckActionState::new),
+    DUAL_WIELD_ACTION(DualWieldAction.class, DualWieldActionState::new),
+    EXHAUST_ACTION(ExhaustAction.class, ExhaustActionState::new),
+    EXHUME_ACTION(ExhumeAction.class, ExhumeActionState::new),
+    FETCH_ATTACK_ACTION(AttackFromDeckToHandAction.class, AttackFromDeckToHandActionState::new),
+    FETCH_SKILL_ACTION(SkillFromDeckToHandAction.class, SkillFromDeckToHandActionState::new),
+    SEEK_ACTION(SeekAction.class, SeekActionState::new);
 
     public Function<AbstractGameAction, CurrentActionState> factory;
     public Class<? extends AbstractGameAction> actionClass;
